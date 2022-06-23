0aa3120d2e676e3f0c4ec27456b596c37eb19476
EzraEzeiru <ezeiru.ezra@gmail.com>
2022-06-23 02:25:21 +0100

    Complete URL Shortener

---
+++ characters.py
@@ -0,0 +1,7 @@
+import string
+
+lower_alphabet_string = string.ascii_lowercase
+upper_alphabet_string = string.ascii_uppercase
+lower_alphabet_list = list(lower_alphabet_string)
+upper_alphabet_list = list(upper_alphabet_string)
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
